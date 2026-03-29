505157f497d1b445ef33fd874333b5e88f1e0774
Lisoing Sem <lisoingsem@gmail.com>
2026-03-30 01:59:23 +0700

    refactor: update authentication middleware from 'sanctum' to 'api' in routes

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,6 @@ declare(strict_types=1);
 use Illuminate\Support\Facades\Route;
 use Modules\Accounting\Http\Controllers\AccountingController;
 
-Route::middleware(['auth:sanctum'])->prefix('v1')->group(function (): void {
+Route::middleware(['auth:api'])->prefix('v1')->group(function (): void {
     Route::apiResource('accountings', AccountingController::class)->names('accounting');
 });
